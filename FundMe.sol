0fecaf21087e639e5198dc2d17882baf9cb3cec3
Sameer Ali <111649727+SameerAli03@users.noreply.github.com>
2022-12-11 16:01:33 +0530

    Add files via upload
    FundMe.sol Updated.

--- FundMe.sol
+++ FundMe.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
 
     uint256 minimumUSD = 50;
@@ -9,8 +11,26 @@ contract FundMe{
         require(msg.value >= minimumUSD, "Not Enough ETH!");
     }
 
-    function withdraw() public{
-
+    function getPrice() public view returns (uint256){
+        // Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (, int price, , ,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
     }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+    }
+
+    function getCoversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+    // function withdraw() public{
+
+    // }
 
 }
