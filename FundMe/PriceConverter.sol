04916a90a0c1352be5fddaa16048b77db5b0fe17
Sameer Ali <111649727+SameerAli03@users.noreply.github.com>
2022-12-13 21:36:14 +0530

    Add files via upload

---
+++ FundMe/PriceConverter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter{
+
+    function getPrice() internal view returns (uint256){
+        // Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (, int price, , ,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getVersion() internal view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        return priceFeed.version();
+    }
+
+    function getCoversionRate(uint256 ethAmount) internal view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+}
