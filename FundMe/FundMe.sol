6f5cbfdf537989c4e80337a6edb5f192ed8820fd
Sameer Ali <111649727+SameerAli03@users.noreply.github.com>
2022-12-14 12:23:21 +0530

    Add files via upload

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -18,8 +18,24 @@ contract FundMe{
         addressToAmountFunded[msg.sender] = msg.value;
     }
 
-    // function withdraw() public{
+    function withdraw() public{
+        for(uint256 funderIndex = 0; funderIndex = funder.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-    // }
+        funders = new address[](0);
+
+        // Transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        // Send
+        bool sendSuccess = payable(msg.sender).transfer(address(this).balance);
+        require(sendSuccess, "Send Failed!");
+
+        // Call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
+    }
 
 }
