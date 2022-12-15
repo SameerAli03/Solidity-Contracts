ea280261cb4f28bc07a59b8f67ae500bea8f6c29
Sameer Ali <111649727+SameerAli03@users.noreply.github.com>
2022-12-15 12:44:53 +0530

    Add files via upload

---
+++ FundMe.sol
@@ -0,0 +1,52 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./PriceConverter.sol";
+
+contract FundMe{
+
+    using PriceConverter for uint256;
+
+    uint256 minimumUSD = 50;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function fund() public payable{
+        require(msg.value.getCoversionRate() >= minimumUSD, "Not Enough ETH!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner{
+        for(uint256 funderIndex = 0; funderIndex = funder.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+    modifier onlyOwner(){
+        require(msg.sender == Owner, "Sender is not the Owner!");
+        _;
+    }
+
+        funders = new address[](0);
+
+        // Transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        // Send
+        bool sendSuccess = payable(msg.sender).transfer(address(this).balance);
+        require(sendSuccess, "Send Failed!");
+
+        // Call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed!");
+    }
+
+}
